43414b8d1783513a51e1761150420ad219b2c80a
k.leshchuk <k.leshchuk@mail.astondevs.ru>
2026-08-24 17:52:20 +0400

    feat: render collections list

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,22 @@
-export const App = () => <div>Collection</div>;
+import { getCollections } from "./api/collections/getCollections";
+import { useFetch } from "./hooks/useFetch";
+
+export const App = () => {
+  const {
+    data: collectionsResponse,
+    loading,
+    error,
+  } = useFetch(() => getCollections({ limit: 10 }));
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+  if (!collectionsResponse) return null;
+
+  return (
+    <ul>
+      {collectionsResponse.data.map(({ id, title }) => (
+        <li key={id}>{title}</li>
+      ))}
+    </ul>
+  );
+};
