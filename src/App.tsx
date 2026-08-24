a9f3b17e23028497c2b655e4a655595901e8ba1e
k.leshchuk <k.leshchuk@mail.astondevs.ru>
2026-08-24 18:42:26 +0400

    feat: add router with collection page

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
-import { getCollections } from "./api/collections/getCollections";
-import { useFetch } from "./hooks/useFetch";
+import { Route, Routes } from "react-router-dom";
+import { CollectionsPage } from "./pages/CollectionsPage/CollectionsPage";
 
-export const App = () => {
-  const {
-    data: collectionsResponse,
-    loading,
-    error,
-  } = useFetch(() => getCollections({ limit: 10 }));
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
-  if (!collectionsResponse) return null;
-
+export function App() {
   return (
-    <ul>
-      {collectionsResponse.data.map(({ id, title }) => (
-        <li key={id}>{title}</li>
-      ))}
-    </ul>
+    <Routes>
+      <Route path="/" element={<CollectionsPage />} />
+    </Routes>
   );
-};
+}
---
+++ src/pages/CollectionsPage/CollectionsPage.tsx
@@ -0,0 +1,22 @@
+import { getCollections } from "../../api/collections/getCollections";
+import { useFetch } from "../../hooks/useFetch";
+
+export const CollectionsPage = () => {
+  const {
+    data: collectionsResponse,
+    loading,
+    error,
+  } = useFetch(() => getCollections({ limit: 10 }));
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+  if (!collectionsResponse) return null;
+
+  return (
+    <ul>
+      {collectionsResponse.data.map(({ id, title }) => (
+        <li key={id}>{title}</li>
+      ))}
+    </ul>
+  );
+};
