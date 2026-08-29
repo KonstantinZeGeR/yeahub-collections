e69413023fb7427b9caa53e09f97f6d7695b7f55
k.leshchuk <k.leshchuk@mail.astondevs.ru>
2026-08-29 16:49:34 +0400

    feat: add pagination

--- src/pages/CollectionsPage/CollectionsPage.tsx
+++ src/pages/CollectionsPage/CollectionsPage.tsx
@@ -1,24 +1,39 @@
+import { useState } from "react";
 import { getCollections } from "../../api/collections/getCollections";
 import { CollectionCard } from "../../components/CollectionCard/CollectionCard";
 import { useFetch } from "../../hooks/useFetch";
 import styles from "./CollectionsPage.module.css";
+import { Pagination } from "../../components/Pagination/Pagination";
+
+const LIMIT = 10;
 
 export function CollectionsPage() {
+  const [page, setPage] = useState(1);
+
   const {
     data: collectionsResponse,
     loading,
     error,
-  } = useFetch(() => getCollections({ limit: 10 }));
+  } = useFetch(() => getCollections({ page, limit: LIMIT }), [page]);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
   if (!collectionsResponse) return null;
 
+  const totalPages = Math.ceil(collectionsResponse.total / LIMIT);
+
   return (
-    <ul className={styles.list}>
-      {collectionsResponse.data.map((collection) => (
-        <CollectionCard key={collection.id} collection={collection} />
-      ))}
-    </ul>
+    <>
+      <ul className={styles.list}>
+        {collectionsResponse.data.map((collection) => (
+          <CollectionCard key={collection.id} collection={collection} />
+        ))}
+      </ul>
+      <Pagination
+        currentPage={page}
+        totalPages={totalPages}
+        onPageChange={setPage}
+      />
+    </>
   );
 }
